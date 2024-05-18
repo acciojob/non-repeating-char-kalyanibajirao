7570ebc0171ee103e7b65ee6c75d23a31a2c59e5
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-05-18 11:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]] === 1) {
+            return str[i];
+        }
+    }
+
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
